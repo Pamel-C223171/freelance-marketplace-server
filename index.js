4a1df6cc81ec40058cb70c947019a9344b5bc0f4
Pamel-C223171 <Mohammadpamel12345@gmail.com>
2025-11-18 09:48:56 +0300

    update to the index.js file

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}G@cluster0.zyhkinn.mongodb.net/?appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zyhkinn.mongodb.net/?appName=Cluster0`;
 
 const client = new MongoClient(uri, {
     serverApi: {
